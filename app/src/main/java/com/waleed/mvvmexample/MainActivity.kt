bed7fce407e1455c3a60c84795eb1791f2fe232f
Waleed <waleed.developer222@gmail.com>
2023-03-02 00:54:24 +0200

    commit

--- app/src/main/java/com/waleed/mvvmexample/MainActivity.kt
+++ app/src/main/java/com/waleed/mvvmexample/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.waleed.mvvmexample.viewModel.CalViewModel
 
